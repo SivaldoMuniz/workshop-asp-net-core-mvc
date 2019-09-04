823174c1d1e1adb720ec6e14e0e62a1aff1488b0
SivaldoMuniz <sivaldo5@hotmail.com>
2019-09-03 22:55:46 -0300

    Implementing grouping search

--- SalesWebMvc/Controllers/SalesRecordsController.cs
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -9,11 +9,11 @@ namespace SalesWebMvc.Controllers
 {
     public class SalesRecordsController : Controller
     {
-        private readonly SalesRecordService _salesRecordsService;
+        private readonly SalesRecordService _salesRecordService;
 
-        public SalesRecordsController(SalesRecordService salesRecordsService)
+        public SalesRecordsController(SalesRecordService salesRecordService)
         {
-            _salesRecordsService = salesRecordsService;
+            _salesRecordService = salesRecordService;
         }
 
         public IActionResult Index()
@@ -34,13 +34,26 @@ namespace SalesWebMvc.Controllers
             ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
             ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
 
-            var result = await _salesRecordsService.FindByDateAsync(minDate, maxDate);
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+   
+             public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+           
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,6 +1,6 @@
 ﻿using SalesWebMvc.Models.Enums;
 using System;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
@@ -8,8 +8,13 @@ namespace SalesWebMvc.Models
     {
 
         public int  Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
        
